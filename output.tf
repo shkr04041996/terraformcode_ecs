6a759119100168555bdf600d3f0ac1ac077b9b57
shkr04041996 <122357586+shkr04041996@users.noreply.github.com>
2024-05-31 17:02:30 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,3 @@
-### Output of Permission ###
-variable "execution_role_arn" {
-  type    = string
-  default = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-variable "task_role_arn" {
-  type    = string
-  default = "arn:aws:iam::353211646521:role/ecsTaskExecutionRole-dev"
-}
-### Output of ALB ###
-variable "target_group_arn" {
-  type    = string
-  default = "arn:aws:elasticloadbalancing:us-east-1:353211646521:targetgroup/MMM-infra-dev-target-group/83624f32de1d975c"
-}
- 
-
 output "ECS_cluster_ID" {
   value = aws_ecs_cluster.main.id
 }
