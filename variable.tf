ff855dc2086164bb96452f89b3d569fc48732e70
shkr04041996 <122357586+shkr04041996@users.noreply.github.com>
2024-05-31 12:16:03 +0530

    Update variable.tf
    region issues

--- variable.tf
+++ variable.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key"{
+  description = "AWS access key"
+  type = string
+  default= ""
+}
+variable "aws_secret_key"{
+  description = "AWS secret key"
+  type = string
+  default = ""
+}
 variable "config" {
   type = object({
     environment = string
